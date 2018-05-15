931453532dafc2c3b7e20b10be6e38947ff0d9cf
amazingskyline <15002963138@163.com>
2018-05-16 00:07:43 +0800

    commit main files

---
+++ tunshu/tunshu/routing.py
@@ -0,0 +1,8 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+import message.routing
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter(
+        message.routing.websocket_urlpatterns
+    )
+})
